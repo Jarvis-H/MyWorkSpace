c396cbd3ce77899e79bbae7de552af342220d014
hepeng01668909 <01668909@yto.net.cn>
2021-04-01 19:48:38 +0800

    Spring注解驱动

--- spring-annotation/src/test/java/test/IOCTest.java
+++ spring-annotation/src/test/java/test/IOCTest.java
@@ -1,15 +1,20 @@
-package com.atguigu.test;
+package test;
 
 import com.atguigu.config.MainConfig;
+import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class IOCTest {
 
+    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
 
+    @Test
+    public void test02() {
+
+    }
 
     @Test
     public void test01() {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
         String[] definitionNames = applicationContext.getBeanDefinitionNames();
         for (String name : definitionNames) {
             System.out.println(name);
